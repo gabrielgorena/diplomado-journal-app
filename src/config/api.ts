01d1125b53cd1b091bd9e046d2451f12f14181a6
gabriel <gabrielgorena.bo@gmail.com>
2025-05-19 21:47:22 -0400

    edit BASE_URL

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // TODO: get from env
-const BASE_URL = 'https://diplomado-journal-api-main-m9mort.laravel.cloud/api';
+const BASE_URL = 'https://diplomado-journal-services-2-main-mjdnei.laravel.cloud/api';
 
 export const api = axios.create({
   baseURL: BASE_URL
